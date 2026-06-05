c4bdf09667798636bb1c8a1b86d11e4543f2d035
Dorel Ciocanu <emailul_tau_github@example.com>
2026-06-05 08:44:50 +0300

    chore: eliminate build cache from git tracking and apply ignore rules

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NgARyGNeHAVlMArWK9Aa0AdCHNulfn9kI4YnN5CSSFo=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_0qawgjk._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_ConstellationView_tsx_0bsq3zf._.js","static/chunks/_01dwca~._.js"],
-    source: "dynamic"
-});
